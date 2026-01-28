803fb657427f13c6e7a076ba13b24328dff56859
Storerooms <139324706+Storerooms@users.noreply.github.com>
2026-01-28 15:05:20 +1100

    Folder restructure and notifications system

---
+++ data/notifications.js
@@ -0,0 +1,7 @@
+const NOTIFICATIONS = [
+  {
+    "type": "warning",
+    "isDark": true,
+    "content": "**Note:** Timetables/classes have not been uploaded to Nexus, so it is not possible to export them yet."
+  }
+];
