fc0775c5c90dfd60945c9d6718d823d2ce683b2a
w-hol <william@holliday.com.au>
2026-02-03 22:18:21 +1100

    Update notifications

--- data/notifications.js
+++ data/notifications.js
@@ -1,12 +1,1 @@
-const NOTIFICATIONS = [
-  {
-    "type": "info",
-    "isDark": true,
-    "content": "**Note:** Timetables/classes have only been uploaded to Nexus for some people, as far as I know"
-  },
-  {
-    "type": "info",
-    "isDark": true,
-    "content": "There is currently no button for the calendar page, but you can just add '/calender' to the end of the base url to get there"
-  }
-];
+const NOTIFICATIONS = [];
